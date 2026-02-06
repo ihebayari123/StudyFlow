d29110e680ef0fe889db9973cf6a85ea5427fd74
Aiheb1 <ayari.iheb1@gmail.com>
2026-02-06 01:31:50 +0100

    CREATE (backoffice)

--- src/Controller/CoursesController.php
+++ src/Controller/CoursesController.php
@@ -68,6 +68,68 @@ public function updatecours($id, Request $req, ManagerRegistry $m, CoursReposito
     $form->handleRequest($req);
 
     if($form->isSubmitted() && $form->isValid()) {
+        $em->flush();  
+        return $this->redirectToRoute('app_showcourses');
+    }
+    
+    return $this->render('courses/updatecourses.html.twig', [
+        'f' => $form,
+    ]);
+}
+
+#[Route('/showcoursesback', name: 'app_showcoursesback')]
+    public function showcoursesback(CoursRepository $coursrepo): Response
+    {
+        $a=$coursrepo->findAll();
+        //$a=$authorrepo->findbyusername();
+       // $a=$authorrepo->trieDescUsername();
+        //$a=$coursrepo->trieAcsUsername();
+        
+        return $this->render('courses/courses.html.twig', [
+            'listcours' => $a,
+        ]);
+    }
+
+#[Route('/addcoursback', name: 'app_addcoursback')]
+    public function addcoursback(Request $req,ManagerRegistry $m): Response
+    {
+
+        $em=$m->getManager();
+        $cours=new cours();
+        $form=$this->createForm(CoursesType::class,$cours);
+        $form->handleRequest($req);
+
+
+     if($form->isSubmitted() && $form->isValid()){
+        $em->persist($cours);
+        $em->flush();
+        return $this->redirectToRoute('app_showcourses');
+     }
+         return $this->render('courses/addcoursesback.html.twig', [
+            'f' => $form,
+        ]);
+    }
+
+#[Route('/deletecoursback/{id}', name: 'app_deletecoursback')]
+    public function deletecoursback($id,ManagerRegistry $m,CoursRepository $coursrepo): Response
+    {
+
+        $em=$m->getManager();
+        $del=$coursrepo->find($id);
+        $em->remove($del);
+        $em->flush();
+        return $this->redirectToRoute('app_showcourses');
+    }
+
+    #[Route('/updatecoursback/{id}', name: 'app_updatecoursback')]
+public function updatecoursback($id, Request $req, ManagerRegistry $m, CoursRepository $coursrepo): Response
+{
+    $em = $m->getManager();
+    $cours = $coursrepo->find($id);
+    $form = $this->createForm(CoursesType::class, $cours);
+    $form->handleRequest($req);
+
+    if($form->isSubmitted() && $form->isValid()) {
         $em->flush();  // No need for persist on existing entity
         return $this->redirectToRoute('app_showcourses');
     }
@@ -76,4 +138,7 @@ public function updatecours($id, Request $req, ManagerRegistry $m, CoursReposito
         'f' => $form,
     ]);
 }
+
 }
+
+
