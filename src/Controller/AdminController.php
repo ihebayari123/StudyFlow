0f3551b73b76faf9214c14daa99c6fcae355445a
mbhamohda <benhamoudam772@gmail.com>
2026-02-07 23:24:09 +0100

    Ajout et modification templates

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Repository\QuestionRepository;
+use App\Repository\QuizRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Question;
+use App\Entity\Quiz;
+use App\Form\QuestionType; 
 
 final class AdminController extends AbstractController
 {
