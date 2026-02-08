76d6c41fc7123614d77359418853ac1483230d6c
Aiheb1 <ayari.iheb1@gmail.com>
2026-02-08 12:07:06 +0100

    WIP: local changes after merge

--- migrations/Version20260205212228.php
+++ migrations/Version20260205212228.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260206012950 extends AbstractMigration
+final class Version20260205212228 extends AbstractMigration
 {
     public function getDescription(): string
     {
